f616573045c78904dce5cc80cfe249cf40d5422e
Rohit Kumar <123806062+rohitc154@users.noreply.github.com>
2025-04-09 11:08:04 +0530

    initial commit

---
+++ build/precache-manifest.f66f517cdde19b201a24c88e5a7bd8a5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1b0ca0c7ab8e7d09fb7c3eaf4b651be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2848242f1c335326de3c",
+    "url": "/static/css/main.cef11367.chunk.css"
+  },
+  {
+    "revision": "001b67d05a335e326bff",
+    "url": "/static/js/2.6a7fe4e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6a7fe4e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2848242f1c335326de3c",
+    "url": "/static/js/main.67d52437.chunk.js"
+  },
+  {
+    "revision": "30259e7a30d4e8b4ab16",
+    "url": "/static/js/runtime-main.a9559881.js"
+  },
+  {
+    "revision": "0a218296b39a3569fe9c933e3ababb49",
+    "url": "/static/media/WeatherIcons.0a218296.gif"
+  },
+  {
+    "revision": "273740c1c2b6baf5d18b8eaa0c8fa273",
+    "url": "/static/media/background.273740c1.jpg"
+  },
+  {
+    "revision": "4dc82f050ab54a60d4367c3fcf0e88e9",
+    "url": "/static/media/city.4dc82f05.jpg"
+  }
+]);
---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "435d2fab4b84c1fe1b5ff7ae22ce938e",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
